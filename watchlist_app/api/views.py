5c5670a61c7d87ca56eb978ea816a308b8e01086
Jaegu Kang <kjaegu9771@naver.com>
2021-07-05 15:34:49 +0900

    DRF Login URL추가 및 리뷰 create 유저추가

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -19,13 +20,21 @@ Class Base View
 
 
 class ReviewCreateAPI(generics.CreateAPIView):
+    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
     # 특정 영화에 대한 리뷰를 생성하기 위함
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
-        movie = WatchList.objects.get(pk=pk)
-        serializer.save(watchlist=movie)
+        watchlist = WatchList.objects.get(pk=pk)
+
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError('You have already reviewed this movie!')
+
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 
 class ReviewListAPI(generics.ListAPIView):
