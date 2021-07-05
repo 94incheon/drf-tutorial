5e5b1ffb1d39919a52100dd0213fadbd4095de9a
Jaegu Kang <kjaegu9771@naver.com>
2021-07-05 19:28:23 +0900

    회원가입 기능구현, serializer에서 유효성검사중

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from rest_framework.authtoken.views import obtain_auth_token
 
+from user_app.api import views
+
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('register/', views.registration_view, name='register'),
 ]
---
+++ user_app/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from user_app.api.serializers import RegistrationSerializer
+
+
+@api_view(['POST'])
+def registration_view(request):
+
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
