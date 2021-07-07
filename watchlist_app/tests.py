1bf9ea5dd3872faf3b1a4ff496e84fa0743c15c2
Jaegu Kang <kjaegu9771@naver.com>
2021-07-07 21:19:18 +0900

    TDD > Watchlist > admin만 create 가능 및 계정갯수 등등

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -42,3 +42,44 @@ class StreamPlatformTestCase(APITestCase):
     def test_streamplatform_detail(self):
         response = self.client.get(reverse('streamplatform-detail', args=(self.stream.id, )))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class WatchListTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='example', password='qwer1234!')
+        self.data = {
+            'username': 'example',
+            'password': 'qwer1234!'
+        }
+        response = self.client.post(reverse('token_obtain_pair'), self.data)
+        self.refresh_token = response.data.get('refresh')
+        self.access_token = response.data.get('access')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+        self.stream = models.StreamPlatform.objects.create(name='Netflix',
+                                                           about='#1 Platform',
+                                                           website='https://www.netflix.com')
+        self.watchlist = models.WatchList.objects.create(platform=self.stream,
+                                                         title='Example Movie!',
+                                                         storyline='Example Movie',
+                                                         active=True)
+
+    def test_watchlist_create(self):
+        data = {
+            'platform': self.stream,
+            'title': 'Example Movie',
+            'storyline': 'Example Story',
+            'active': True
+        }
+        response = self.client.post(reverse('watchlist-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('watchlist-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_watchlist_detail(self):
+        response = self.client.get(reverse('watchlist-detail', args=(self.watchlist.id, )))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.get().title, 'Example Movie!')
+        self.assertEqual(User.objects.count(), 1)
