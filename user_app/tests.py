050b6f687ff5f3c07eb5ab002472f1f7eea9677f
Jaegu Kang <kjaegu9771@naver.com>
2021-07-07 17:30:42 +0900

    나의 첫 TDD - test_함수명, 테스트 코드 작성방법

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+'''
+실제 DB를 사용하지 않으므로 안심할것
+자동으로 Test DB가 생성되고 다시 삭제된다.
+`python manage.py test <app_name>`
+'''
+
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data = {
+            'username': 'testcase',
+            'email': 'testcase@example.com',
+            'password': 'qwer1234!',
+            'password2': 'qwer1234!',
+        }
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # 값이 서로 틀리면 Error!
